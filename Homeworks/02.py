9c1abb0104d6d4a7496cace78d73ab1c1c8f9a6f
Milan Mitrovic <58112365+milanzmitrovic@users.noreply.github.com>
2023-02-13 22:29:39 +0100

    Create 02.py

---
+++ Homeworks/02.py
@@ -0,0 +1,75 @@
+
+# 1. Create list with numbers from 0 to 20.
+#    Name list as list_1.
+
+# 2. Create list with numbers from 20 to 0.
+#    Name list as list_2.
+
+# 3. Add previously created two lists.
+
+# 4. Multiply list_1 6 times.
+#    What you got?
+
+# 5. Subtract 5 from each element in list_2.
+#    Create list_3 that will hold results.
+
+# 6. Create alias i.e. assign nickname to list_1.
+#    New nickname should be list_alias_1.
+#    Prove that you did exercise correctly.
+
+# 7. Create list that have same elements as list_2,
+#    but it should have different ID.
+#    Prove that you did exercise correctly.
+
+# 8. Create list_a that has values from 200 to 300.
+#    Create list_b that has values from 500 to 400.
+#    Create list_c in following fashion:\
+#       list_a = [1,2,3]
+#       list_b = [4,5,6]
+#       list_c = [5,7,9]
+
+# 9. Create list_1 with numbers from 100 to 400.
+#    Create list_2 that will have structure like:
+#    list_1 = [1,2,3,4]
+#    list_2 = [1,2,9,16]
+
+# 10. Create list that will have 30 elements.
+#     Each element should be random number between 0 and 77.
+
+# 11. Create list with arbitrary data types (str, int, float,...)
+
+# 12. Create nested list i.e. list with lists inside.
+
+# 13. Create list_1 with arbitrary strings inside it.
+#     Append new string elements to list_1.
+#     Check if ID of list_1 has been changed?
+
+# 14. Remove first and last element in list_1 from
+#     previous exercise.
+#     Check if ID of list_1 has been changed?
+
+# 15. Create my_list with numbers from 100 to 200.
+#     Loop through list and print each element
+#     multiplied by 3.
+
+# 16. Research about 'if statements' in Python.
+#     Loop through my_list from previous exercise,
+#     even elements put in even_list and odd elements
+#     put in odd_list.
+
+# 17. Create some example with 'copy' function.
+#     Show if ID is being changed after modifying
+#     elements of list.
+
+# 18. Create some arbitrary list. 
+#     Delete all elements from list.
+#     CHeck if ID of list has been changed?
+
+
+# 19. Create some arbitrary list.
+#     Delete first and last element of list.
+#     Check if ID of list was changed?
+
+# 20. Try to find some method of list
+#     class that we haven't been using.
+#     Show some example with that method.
